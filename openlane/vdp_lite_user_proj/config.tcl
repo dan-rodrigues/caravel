b1ac1b1ed2c3a12c948abf883cf1d66921a57441
Dan Rodrigues <danrr.gh.oss@gmail.com>
2020-12-05 16:04:42 +1100

    Relocate core vdp-lite sources to verilog/rtl config.tcl slightly cleaned up in the process

--- openlane/vdp_lite_user_proj/config.tcl
+++ openlane/vdp_lite_user_proj/config.tcl
@@ -1,4 +1,5 @@
 set script_dir [file dirname [file normalize [info script]]]
+set vdp_rtl_dir "$script_dir/../../verilog/rtl/vdp_lite_user_proj/"
 
 set ::env(DESIGN_NAME) vdp_lite_user_proj
 
@@ -9,22 +10,22 @@ set ::env(GLB_RT_MAXLAYER) 5
 set ::env(VERILOG_FILES) "\
     $script_dir/../../verilog/rtl/defines.v \
     $script_dir/../../verilog/rtl/user_project_wrapper.v \
-    $script_dir/../../vdp/vdp.v \
-    $script_dir/../../vdp/vdp_host_interface.v \
-    $script_dir/../../vdp/vdp_vga_timing.v \
-    $script_dir/../../vdp/vdp_sprite_core.v \
-    $script_dir/../../vdp/vdp_sprite_raster_collision.v \
-    $script_dir/../../vdp/vdp_vram_bus_arbiter_standard.v \
-    $script_dir/../../vdp/vdp_sprite_render.v \
-    $script_dir/../../vdp/vdp_priority_compute.v \
-    $script_dir/../../vdp/vdp_layer_priority_select.v \
-    $script_dir/../../vdp/ffram.v \
-    $script_dir/../../vdp/delay_ff.v \
-    $script_dir/../../vdp/delay_ffr.v \
-    $script_dir/../../verilog/rtl/vdp_lite_user_proj/vdp_lite_user_proj.v \
-    $script_dir/../../verilog/rtl/vdp_lite_user_proj/char_rom.v"
+    $vdp_rtl_dir/vdp.v \
+    $vdp_rtl_dir/vdp_host_interface.v \
+    $vdp_rtl_dir/vdp_vga_timing.v \
+    $vdp_rtl_dir/vdp_sprite_core.v \
+    $vdp_rtl_dir/vdp_sprite_raster_collision.v \
+    $vdp_rtl_dir/vdp_vram_bus_arbiter_standard.v \
+    $vdp_rtl_dir/vdp_sprite_render.v \
+    $vdp_rtl_dir/vdp_priority_compute.v \
+    $vdp_rtl_dir/vdp_layer_priority_select.v \
+    $vdp_rtl_dir/ffram.v \
+    $vdp_rtl_dir/delay_ff.v \
+    $vdp_rtl_dir/delay_ffr.v \
+    $vdp_rtl_dir/vdp_lite_user_proj.v \
+    $vdp_rtl_dir/char_rom.v"
 
-set ::env(VERILOG_INCLUDE_DIRS) "$script_dir/../../vdp/"
+set ::env(VERILOG_INCLUDE_DIRS) "$vdp_rtl_dir/"
 
 set ::env(CLOCK_PORT) "wb_clk_i"
 set ::env(CLOCK_PERIOD) "39"
@@ -53,4 +54,3 @@ set ::env(ROUTING_CORES) 6
 #	$script_dir/../../lef/DFFRAM.lef"
 #set ::env(EXTRA_GDS_FILES) "\
 #	$script_dir/../../gds/DFFRAM.gds"
-

9ddcf9311664c14208a2afac8202b56ff096962d
Dan Rodrigues <danrr.gh.oss@gmail.com>
2020-12-05 21:55:45 +1100

    Restore obs_core_obs specify_grid, although it didn't fix the current issue

--- openlane/user_project_wrapper/pdn.tcl
+++ openlane/user_project_wrapper/pdn.tcl
@@ -17,6 +17,17 @@ pdngen::specify_grid stdcell {
     connect {{met4 met5}}
 }
 
+# Only used in user_project_wrapper_empty.def ?
+pdngen::specify_grid macro {
+    instance "obs_core_obs"
+    power_pins $::env(_VDD_NET_NAME)
+    ground_pins $::env(_GND_NET_NAME)
+    blockages "li1 met1 met2 met3 met4 met5"
+    straps { 
+    } 
+    connect {}
+}
+
 pdngen::specify_grid macro {
     power_pins $::env(_VDD_NET_NAME)
     ground_pins $::env(_GND_NET_NAME)
